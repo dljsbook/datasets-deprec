2a4cad08441886d1a553282155ee4aae8d1d126d
Kevin Scott <kevin@hitheory.com>
2018-11-24 21:18:12 -0500

    Update link to github hosting

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const Color = require('color');
 
-export const DATAROOT = 'https://dljsbook.github.io/code/';
+export const DATAROOT = 'https://dljsbook.github.io/datasets/';
 
 export const COLORS = [
   Color.hsl([199, 100, 57]),
